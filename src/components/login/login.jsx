0109b497df7853e303ac53fdc93e81be7dd8dbac
ips3471 <82862685+ips3471@users.noreply.github.com>
2021-07-30 19:07:15 +0900

    root directory comma

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -8,7 +8,7 @@ const Login = ({authService}) => {
     const history = useHistory();
     const goToMaker = userId => {
         history.push({
-            pathname: '/card-maker',
+            pathname: './card-maker',
             state: { id: userId},
         });
     };
